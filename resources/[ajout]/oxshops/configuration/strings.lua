7cb059cea48df2c5f3d0bd1c5718c57778d15601
CtFatalv <contact.fatalv@gmail.com>
2023-05-24 14:18:33 +0200

    Legacy City

--- resources/[ajout]/oxshops/configuration/strings.lua
+++
@@ -1,8 +0,0 @@
-Strings = {
-    sell_price = 'Prix de vente',
-    amount_input = 'Amount',
-    inventory = 'Inventory',
-    success = 'Success',
-    item_stocked_desc = 'Vous avez stocké un article pour $%s!',
-    
-}
