f296909f5cb5c5c0214cbd58877ad72060a84608
CtFatalv <contact.fatalv@gmail.com>
2023-07-01 11:12:00 +0200

    Legacy City

--- resources/[addon]/esx_dmvschool/html/questions.js
+++ resources/[addon]/esx_dmvschool/html/questions.js
@@ -1,91 +1,91 @@
 var tableauQuestion = [
 	{
-		question : "Si vous roulez à 80 km/h et que vous vous approchez d'une zone résidentielle, vous devez : ",
-		propositionA : "Vous accélérez",
-		propositionB : "Vous gardez votre vitesse si vous ne dépassez pas d'autres véhicules",
-		propositionC : "Tu ralentis",
-		propositionD : "Vous gardez votre vitesse",
-		réponse : "C"
+		question:"Si vous roulez à 80 km/h et que vous vous approchez d'une zone résidentielle, vous devez:",
+		propositionA:"Vous accélérez",
+		propositionB:"Vous gardez votre vitesse si vous ne dépassez pas d'autres véhicules",
+		propositionC:"Tu ralentis",
+		propositionD:"Vous gardez votre vitesse",
+		reponse:"C"
 	},
 	
 	{
-		question : "Si vous tournez à droite à un feu de circulation, mais que vous voyez un passage pour piétons, que faites-vous : ",
-		propositionA : "Vous dépassez le piéton",
-		propositionB : "Vous vérifiez qu'il n'y a pas d'autres véhicules dans les environs",
-		propositionC : "Vous attendez que le piéton ait traversé",
-		propositionD : "Vous tirez sur le piéton et continuez à conduire",
-		réponse : "C"
+		question:"Si vous tournez à droite à un feu de circulation, mais que vous voyez un passage pour piétons, que faites-vous:",
+		propositionA:"Vous dépassez le piéton",
+		propositionB:"Vous vérifiez qu'il n'y a pas d'autres véhicules dans les environs",
+		propositionC:"Vous attendez que le piéton ait traversé",
+		propositionD:"Vous tirez sur le piéton et continuez à conduire",
+		reponse:"C"
 	},
 	
 	{
-		question : "Sans indication préalable, la vitesse dans une zone résidentielle est de : __ km/h",
-		propositionA : "30 km/h",
-		propositionB : "50 km/h",
-		propositionC : "40 km/h",
-		propositionD : "60 km/h",
-		réponse : "B"
+		question:"Sans indication préalable, la vitesse dans une zone résidentielle est de:__ km/h",
+		propositionA:"30 km/h",
+		propositionB:"50 km/h",
+		propositionC:"40 km/h",
+		propositionD:"60 km/h",
+		reponse:"B"
 	},
 	
 	{
-		question : "Avant chaque changement de voie, vous devez :",
-		propositionA : "Vérifiez vos rétroviseurs",
-		propositionB : "Vérifiez vos angles morts",
-		propositionC : "Signalez vos intentions",
-		propositionD : "Tout ce qui précède",
-		réponse : "D"
+		question:"Avant chaque changement de voie, vous devez :",
+		propositionA:"Vérifiez vos rétroviseurs",
+		propositionB:"Vérifiez vos angles morts",
+		propositionC:"Signalez vos intentions",
+		propositionD:"Tout ce qui précède",
+		reponse:"D"
 	},
 	
 	{
-		question : "Quel taux d'alcoolémie est classé comme conduite en état d'ébriété ?",
-		propositionA : "0,05 %",
-		propositionB : "0,18 %",
-		propositionC : "0,08 %",
-		propositionD : "0,06 %",
-		réponse : "C"
+		question:"Quel taux d'alcoolémie est classé comme conduite en état d'ébriété ?",
+		propositionA:"0,05 %",
+		propositionB:"0,18 %",
+		propositionC:"0,08 %",
+		propositionD:"0,06 %",
+		reponse:"C"
 	},
 
 	{
-		question : "Quand pouvez-vous continuer à conduire à un feu ?",
-		propositionA : "Quand c'est vert",
-		propositionB : "Quand il n'y a personne au carrefour",
-		propositionC : "Vous êtes en zone scolaire",
-		propositionD : "Quand c'est vert et/ou rouge et que tu tournes à droite",
-		réponse : "D"
+		question:"Quand pouvez-vous continuer à conduire à un feu ?",
+		propositionA:"Quand c'est vert",
+		propositionB:"Quand il n'y a personne au carrefour",
+		propositionC:"Vous êtes en zone scolaire",
+		propositionD:"Quand c'est vert et/ou rouge et que tu tournes à droite",
+		reponse:"D"
 	},
 
 	{
-		question : "Un piéton a un signal interdit de traverser, que faites-vous ?",
-		propositionA : "Vous les avez laissés passer",
-		propositionB : "Vous observez avant de continuer",
-		propositionC : "Vous faites signe pour leur dire de traverser",
-		propositionD : "Vous continuez car votre feu est vert",
-		réponse : "D"
+		question:"Un piéton a un signal interdit de traverser, que faites-vous ?",
+		propositionA:"Vous les avez laissés passer",
+		propositionB:"Vous observez avant de continuer",
+		propositionC:"Vous faites signe pour leur dire de traverser",
+		propositionD:"Vous continuez car votre feu est vert",
+		reponse:"D"
 	},
 
 	{
 		question: "Ce qui est autorisé lors du dépassement d'un autre véhicule",
-		propositionA : "Vous le suivez de près pour le passer plus vite",
-		propositionB : "Vous le dépassez sans quitter la chaussée",
-		propositionC : "Vous roulez à l'opposé de la route pour passer",
-		propositionD : "Vous dépassez la limite de vitesse pour les dépasser",
-		réponse : "C"
+		propositionA:"Vous le suivez de près pour le passer plus vite",
+		propositionB:"Vous le dépassez sans quitter la chaussée",
+		propositionC:"Vous roulez à l'opposé de la route pour passer",
+		propositionD:"Vous dépassez la limite de vitesse pour les dépasser",
+		reponse:"C"
 	},
 
 	{
-		question : "Vous roulez sur une autoroute qui indique une vitesse maximale de 120 km/h. Mais la plupart des trafiquants roulent à 125 km/h, vous ne devez donc pas rouler plus vite que :",
-		propositionA : "120 km/h",
-		propositionB : "125 km/h",
-		propositionC : "130 km/h",
-		propositionD : "110 km/h",
-		réponse : "A"
+		question:"Vous roulez sur une autoroute qui indique une vitesse maximale de 120 km/h. Mais la plupart des trafiquants roulent à 125 km/h, vous ne devez donc pas rouler plus vite que :",
+		propositionA:"120 km/h",
+		propositionB:"125 km/h",
+		propositionC:"130 km/h",
+		propositionD:"110 km/h",
+		reponse:"A"
 	},
 
 	{
-		question : " Lorsque vous êtes dépassé par un autre véhicule, il est important de NE PAS : ",
-		propositionA : "Ralentir",
-		propositionB : "Vérifiez vos rétroviseurs",
-		propositionC : "Regardez les autres conducteurs",
-		propositionD : "Augmentez votre vitesse",
-		réponse : "D"
+		question:" Lorsque vous êtes dépassé par un autre véhicule, il est important de NE PAS:",
+		propositionA:"Ralentir",
+		propositionB:"Vérifiez vos rétroviseurs",
+		propositionC:"Regardez les autres conducteurs",
+		propositionD:"Augmentez votre vitesse",
+		reponse:"D"
 	},
 ]
