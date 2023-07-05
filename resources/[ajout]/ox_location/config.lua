667e3cfa89ad19af1108acbe9db15570d2892f29
CtFatalv <contact.fatalv@gmail.com>
2023-07-05 12:18:59 +0200

    Legacy City

---
+++ resources/[ajout]/ox_location/config.lua
@@ -0,0 +1,46 @@
+Config = {}
+
+-- RENT U GRADU --
+
+Config.RentGradSpawnCoords = vector3(-961.90, -2704.96, 13.83)
+Config.RentGradHeading = 75.0
+Config.RentLicensePlate = 'LOCATION'
+Config.RentGradVozilo1 = 'bmx'
+Config.RentGradVozilo2 = 'cruiser'
+Config.RentGradVozilo3 = 'faggio'
+
+-- RENT U SANDY --
+
+Config.RentSandySpawnCoords = vector3(-235.61, -984.77, 28.62)
+Config.RentSandyHeading = 160.00
+Config.RentSandyVozilo1 = 'bmx'
+Config.RentSandyVozilo2 = 'cruiser'
+Config.RentSandyVozilo3 = 'faggio'
+
+-- RENT U PALETO --
+
+Config.RentPaletoSpawnCoords = vector3(94.94, 6372.73, 30.61)
+Config.RentPaletoHeading = 11.33
+Config.RentPaletoVozilo1 = 'bmx'
+Config.RentPaletoVozilo2 = 'cruiser'
+Config.RentPaletoVozilo3 = 'faggio'
+
+-- PARE --
+
+Config.Vozilo1Naplata = 50
+Config.Vozilo2Naplata = 150
+Config.Vozilo3Naplata = 300
+
+-- BLIP --
+
+Config.BlipGrad = {title="Location de véhicule", colour=2, id=811, x = -959.93, y = -2701.92, z = 13.83}
+Config.BlipSandy = {title="Location de véhicule", colour=2, id=811, x = -253.98, y = -970.84, z = 31.22} 
+Config.BlipPaleto = {title="Location de véhicule", colour=2, id=811, x = 96.13, y = 6363.04, z = 30.36}
+
+-- NPC --
+
+Config.Pedovi = {
+    {-959.93, -2701.92, 12.83, "Ped", 116.42, "a_m_y_eastsa_02", "a_m_y_eastsa_02"},
+    {-253.98, -970.84, 30.22, "Ped", 170.00, "a_m_y_eastsa_02", "a_m_y_eastsa_02"},
+    {96.13, 6363.04, 30.36, "Ped", 22.67, "a_m_y_eastsa_02", "a_m_y_eastsa_02"},
+}
