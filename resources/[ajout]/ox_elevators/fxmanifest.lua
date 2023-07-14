d268779d87e6ca027604828e02b6500738023c97
CtFatalv <contact.fatalv@gmail.com>
2023-07-14 12:12:38 +0200

    Legacy City

---
+++ resources/[ajout]/ox_elevators/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AngelicXS'
+version '3.2.3'
+lua54 'yes'
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
+
+shared_script '@ox_lib/init.lua'
